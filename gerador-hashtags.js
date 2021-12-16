2fcdbb166473a51111beaf59c9d4c6b12d4db7d5
Douglas Aguiar <daguiaralcantara@gmail.com>
2021-12-16 12:16:32 -0300

    Exercício totalmente concluído.

--- gerador-hashtags.js
+++ gerador-hashtags.js
@@ -1,6 +1,14 @@
 function generateHashtag (str) {
     var capitalize = (str) => str[0].toUpperCase() + str.slice(1);
     let semEspaco = str.trim();
+    for (let i = 0; i < semEspaco.length; i++) {
+        if (semEspaco[i] == ' ') {
+            let j = i + 1;
+            while (semEspaco[j] == ' ') {
+                semEspaco = semEspaco.replace(' ', '');
+            }
+        }
+    }
     let palavras = semEspaco.split(' ').map(capitalize);
     let sentence = palavras.join('');
     if (str.length == 0) {
@@ -12,7 +20,6 @@ function generateHashtag (str) {
         return hashtag;
     }
 }
-// Não está funcionando para o caso de vários espaços internos.
 
-let str = ("code" + " " + "wars");
+let str = ("code" + "           " + "wars");
 console.log(generateHashtag(str));
